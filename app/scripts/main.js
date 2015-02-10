d6fa79886e2f7a5097d4e048a0d69600f7bdce22
Shane Blatt <shane.blatt@yahoo.com>
2015-02-09 20:50:38 -0500

    work with handlebars

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,27 @@
 console.log('The Iron Yard Rocks');
+
+var data = {
+  book: 'Moby Dick',
+  author: 'Herman Melville'
+};
+
+// Grab the template
+var source = $('#book').html();
+
+// Compile the Template (creates the function)
+var template = Handlebars.compile(templates);
+
+// Send that data in
+$('.book').html(template(data));
+
+var data = {
+  books : [
+    { book: 'Moby Dick', author: 'Herman Melville' },
+    { book: 'Great Expectations', author: 'Charles Dickens'},
+    { book: 'Where the Red Fern Grows', author: 'Wilson Rawls'}
+  ]
+};
+
+var source = $('#books').html();
+var template = Handlebars.templates['books'];
+$('.books').html(template(data));
